b2d3dc856e50ca14d3cda4f8393cf731ad2120c4
arunkone07 <68457293+arunkone07@users.noreply.github.com>
2021-09-16 07:48:47 +0530

    Update Contains_Duplicate.cpp

--- src/easy/Contains_Duplicate.cpp
+++ src/easy/Contains_Duplicate.cpp
@@ -27,9 +27,11 @@ public:
 //         map<int, bool> mp;
 //         for(int i=0; i<nums.size(); i++)
 //         {
-//             if(mp[nums[i]])
-//                 return true;
-//             mp[nums[i]] = true;
+//             if(mp[nums[i]] == false)
+//                    mp[nums[i]] = true;
+//             else
+//                 return true;     
 //         }
+//         return false;
 //     }
 // };
