8540cd1e73072bc449f624e9d60fac3c59923ac3
arunkone07 <68457293+arunkone07@users.noreply.github.com>
2021-10-07 20:16:34 +0530

    Create merge_intervals.cpp

---
+++ src/medium/merge_intervals.cpp
@@ -0,0 +1,28 @@
+//https://leetcode.com/problems/merge-intervals
+
+class Solution {
+public:
+    vector<vector<int>> merge(vector<vector<int>>& v) {
+        int n = v.size();
+        int mx = 0, j=0;
+        vector<vector<int>> v2;
+        
+        sort(v.begin(), v.end());
+        v2.push_back(v[0]);
+        
+        
+        for(int i=1; i<n; i++)
+        {   
+            if(v[i][0]<=v2[j][1])
+                v2[j][1] = max(v[i][1], v2[j][1]);
+            
+            else
+            {
+                v2.push_back(v[i]);
+                j++;
+            }
+        }
+        
+        return v2;
+    }
+};
